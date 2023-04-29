c5baed600c8e80b9f893d9c71980795ff40b9d93
Jaga1999 <jagasiva1999@gmail.com>
2023-04-29 20:30:55 +0530

    added status codes and some checks

--- WebApi/Controllers/CountryController.cs
+++ WebApi/Controllers/CountryController.cs
@@ -17,40 +17,99 @@ namespace WebApi.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<Country>> GetAll()
         {
-            return _dbcontext.Countries.ToList();
+            var countries = _dbcontext.Countries.ToList();
+
+            if (countries == null)
+            {
+                return NoContent(); 
+            }
+
+            return Ok(countries);
         }
 
         [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<Country> GetById(int id)
         {
-            return _dbcontext.Countries.Find(id);
+            var country = _dbcontext.Countries.Find(id);
+
+            if (country == null)
+            {
+                return NoContent();
+            }
+
+            return Ok(country);
         }
 
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
         public ActionResult<Country> Create([FromBody]Country country)
         {
+            var result = _dbcontext.Countries.AsQueryable().Where(x => x.Name.ToLower().Trim() == country.Name.ToLower().Trim()).Any();
+            if (result)
+            {
+                return Conflict("Country Already Exists in Database");
+            }
+
             _dbcontext.Countries.Add(country);
             _dbcontext.SaveChanges();
-            return Ok();
+            return CreatedAtAction("GetById", new { id = country.Id }, country);
         }
 
-        [HttpPut]
-        public ActionResult<Country> Update([FromBody]Country country)
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Country> Update(int id,[FromBody]Country country)
         {
-            _dbcontext.Countries.Update(country);
+            if (country == null || id != country.Id)
+            {
+                return BadRequest();
+            }
+
+            var countryFromDB = _dbcontext.Countries.Find(id);
+
+            if(countryFromDB == null)
+            {
+                return NotFound();
+            }
+
+            countryFromDB.Name = country.Name;
+            countryFromDB.ShortName = country.ShortName;
+            countryFromDB.CountryCode = country.CountryCode;
+
+            _dbcontext.Countries.Update(countryFromDB);
             _dbcontext.SaveChanges();
-            return Ok();
+            return NoContent();
         }
 
         [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<Country> DeleteById(int id)
         {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
             var country = _dbcontext.Countries.Find(id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
             _dbcontext.Remove(country);
             _dbcontext.SaveChanges();
-            return Ok();
+            return NoContent();
         }
     }
 }
